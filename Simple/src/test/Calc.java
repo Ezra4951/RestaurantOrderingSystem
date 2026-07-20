b905de0a5799aff2ac54ed427f4c5a1cdda69e40
Ezra4951 <Kimdoksoo4951@gmail.com>
2026-07-20 13:52:39 +0800

    Completed!
    Signed-off-by: Ezra4951 <Kimdoksoo4951@gmail.com>

--- Simple/src/test/Calc.java
+++ Simple/src/test/Calc.java
@@ -3,38 +3,63 @@ package test;
 import java.util.Scanner;
 
 public class Calc {
-	
-	static Scanner input = new Scanner(System.in);
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String op = "";
-		while (true) {
+		Scanner input = new Scanner(System.in);
+		
+		boolean run = true;
+		while (run) {
 			System.out.println("===WELCOME TO MY BASIC CALCULATOR===");
 			System.out.println("Type cancel if you want to exit!!!");
 			System.out.print("Enter Operator [+, -, *, /, %]: ");
-			op = input.nextLine();
+			String op = input.nextLine();
+			
+			System.out.print("Enter your First Number: ");
+			double num = input.nextFloat();
+			
+			
+			System.out.print("Enter your Second Number: ");
+			double num2 = input.nextFloat();
+			input.nextLine();
 			
 			 if (op.equalsIgnoreCase("cancel")) {
 				 System.out.println("Calculator Cancelled.... EXITING CALCULATOR! TRY AGAIN!!");
 				 return;
 			 } if (op.matches("[0-9]+")) {
-				 System.out.println("ERROR!!!!! PLSS USE OPERATOR NOT NUMBER!!!!");
+				 System.out.println("ERROR!!!! Restarting.....!!!!\n");
 				 continue;
-			 } if (op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/") || op.equals("%")) {
-				 break;
-			 }
-			 System.out.println("Invalid operator. Please use +, -, *, /, or %.");
-		}
+			  }
+			 
+			 	double result = 0;
 		
-		System.out.print("Enter your First Number: ");
-		double num = input.nextFloat();
+	    	char operator = op.charAt(0);
 		
-		
-		System.out.print("Enter your Second Number: ");
-		double num2 = input.nextFloat();
+	    	boolean valid = true;
 		
 		
-	}
+	    	switch(operator) {
+		case '+':
+			result = num + num2; break;
+		case '-':
+			result = num - num2; break;
+		case '*':
+			result = num * num2; break;
+		case '/':
+			if (num2 != 0) {
+				result = num / num2;
+			} else {
+				System.out.println("SYSTEM ERROR!!! Division by zero!!!!!");
+				valid = false;
+			   } break;
+			   default: 
+				   System.out.println("Invalid operator. Please use +, -, *, or /.");
+	    	} 
+			  if (valid) {
+				  String format = String.format("Result: %.2f\n", result);
+				System.out.println(format);
+			}
+	    }
+		input.close();
+  }
 }
