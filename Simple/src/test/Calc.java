78aea8db591ccd89801e94fdd9a42cae01c58c51
Ezra4951 <Kimdoksoo4951@gmail.com>
2026-07-21 17:02:30 +0800

    Fixed!!!

--- Simple/src/test/Calc.java
+++ Simple/src/test/Calc.java
@@ -7,62 +7,63 @@ public class Calc {
 		// TODO Auto-generated method stub
 		
 		Scanner input = new Scanner(System.in);
-		
-		boolean run = true;
-		while (run) {
-			System.out.println("===WELCOME TO MY BASIC CALCULATOR===");
-			System.out.println("Type cancel if you want to exit!!!");
-			System.out.print("Enter Operator [+, -, *, /, %]: ");
-			String op = input.nextLine();
-			
-			 if (op.equalsIgnoreCase("cancel")) {
-				 System.out.println("Calculator Cancelled.... EXITING CALCULATOR!\n"
-				 		+ " THANK YOU FOR USING MY BASIC CALCULATOR!!");
-				 return;
-			 } if (op.matches("[0a-zA-Z0-9]+")) {
-				 System.out.println("ERROR!!!! Restarting.....!!!!\n");
-				 continue;
-			  }
-		
-			
-			System.out.print("Enter your First Number: ");
-			double num = input.nextFloat();
 				
-			System.out.print("Enter your Second Number: ");
-			double num2 = input.nextFloat();
-			input.nextLine();
-			 
-			 	double result = 0;
-		
-	    	char operator = op.charAt(0);
-		
-	    	boolean valid = true;
-		
-		
-	    	switch(operator) {
-		case '+':
-			result = num + num2; break;
-		case '-':
-			result = num - num2; break;
-		case '*':
-			result = num * num2; break;
-		case '/':
-			if (num2 != 0) {
-				result = num / num2;
-			} else {
-				System.out.println("SYSTEM ERROR!!! Division by zero!!!!!");
-				valid = false;
-			   } break;
-		case '%':
-			result = num % num2; break;
-			   default: 
-				   System.out.println("Invalid operator. Please use +, -, *, or /.");
-	    	} 
-			  if (valid) {
-				  String format = String.format("Result: %.2f\n", result);
-				System.out.println(format);
-			}
-	    }
-		input.close();
+			boolean run = true;
+			while (run) {
+				System.out.println("===WELCOME TO MY BASIC CALCULATOR===");
+				System.out.println("Type cancel if you want to exit!!!");
+				System.out.print("Enter Operator [+, -, *, /, %]: ");
+				String op = input.nextLine();
+				
+				 if (op.equalsIgnoreCase("cancel")) {
+					 System.out.println("Calculator Cancelled.... EXITING CALCULATOR!\n"
+					 		+ " THANK YOU FOR USING MY BASIC CALCULATOR!!");
+					 return;
+				 } if (op.matches("[0a-zA-Z0-9]+")) {
+					 System.out.println("ERROR!!!! Restarting.....!!!!\n");
+					 continue;
+				  }
+			
+				
+				System.out.print("Enter your First Number: ");
+				double num = input.nextFloat();
+					
+				System.out.print("Enter your Second Number: ");
+				double num2 = input.nextFloat();
+				input.nextLine();
+				 
+				 	double result = 0;
+			
+				char operator = op.charAt(0);
+			
+				boolean valid = true;
+			
+			
+				switch(operator) {
+			case '+':
+				result = num + num2; break;
+			case '-':
+				result = num - num2; break;
+			case '*':
+				result = num * num2; break;
+			case '/':
+				if (num2 != 0) {
+					result = num / num2;
+				} else {
+					System.out.println("SYSTEM ERROR!!! Division by zero!!!!!");
+					valid = false;
+				   } break;
+			case '%':
+				result = num % num2; break;
+				   default: 
+					   System.out.println("Invalid operator. Please use +, -, *, or /.");
+				} 
+				  if (valid) {
+					  String format = String.format("Result: %.2f\n", result);
+					System.out.println(format);
+				}
+				  input.close();
+			 }	
+		}
   }
-}
+
